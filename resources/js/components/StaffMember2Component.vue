f316164a39e1b1a16318a099984ba47b6a469852
vagrant <vagrant@homestead>
2018-11-07 20:59:37 +0000

    Added some of the cals

--- resources/js/components/StaffMember2Component.vue
+++ resources/js/components/StaffMember2Component.vue
@@ -39,32 +39,62 @@
 
         computed: {       
 
+            dailyWage() {
+                // hours pd * basic wage
+                return this.employee.hours_pd * this.employee.basic_wage;
+            },
+
+            basicSalary() {
+                //  ((daily wage * days pw) *52) / 12
+                return ((this.dailyWage * this.employee.days_pw) * 52) / 12;
+            },
+
+            wagePcm() {
+
+                // (((Hours_pd x Days_pw) x 52) /12) x Basic_wage
+                return (((this.employee.hours_pd * this.employee.days_pw) * 52) / 12) * this.employee.basic_wage;
+            },
+
+            daysPcm() {
+                // how is working days pcm calculated?
+            },
+
             commissionTarget() {
-
-            },
-
-            monthlyWage() {
-
-            },
-
-            finalTarget() {
-
+                // Wage PCM x Target Multiplier
+                return this.wagePcm * this.employee.target_multiplier;
             },
 
             deductSick() {
-
+                // ((Hours _PD x Basic_Wage) x Sick Days) - Wage PCM
+                return ((this.emplyee.hours_pd * this.employee.basic_wage) * this.sick) - this.wagePcm;
             },
 
-            commissionSum() {
+            // PRE BOOKED HOLS VALUE??
 
+            finalTarget() {
+                // (Commission Target / Number of working Days PCM) x (Number of working days PCM - pre booked Hols)
+                
             },
 
             commissionAchieved() {
+                // ((Services + (Products /2)  = Total Revenue
 
+                // If Total Revenue is Greater than Final Target then 
+                // ((Total Revenue - FINAL_TARGET) / 100) * Percentage_Return = commission achieved
+
+                // If Total Revenue is Less Then show NULL
+
+                var totalRev = ((this.services + (this.products / 2);
+
+                if (totalRev > this.finalTarget) {
+                   return ((totalRev - this.finalTarget) / 100) * this.employee.percentage_return;
+                }
+
+                return NULL; 
             },
 
             wage() {
-                return 100;
+                
             }
         
         }
